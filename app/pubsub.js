5aac0c24dbca71a0ff6b97e81027872e1d95ea18
Daniel Atuma <atumadann@gmail.com>
2021-10-20 14:15:27 +0100

    Exposed a new API method to read the transaction pool data.
    Broadcasted transactions as they occurred through the API.
    
    Synced the transaction pool map as a network-dependent object like the blockchain.

--- app/pubsub.js
+++ app/pubsub.js
@@ -2,13 +2,15 @@ const redis = require('redis');
 
 const CHANNELS = {
     TEST : 'TEST',
-    BLOCKCHAIN: 'BLOCKHAIN'
+    BLOCKCHAIN: 'BLOCKHAIN',
+    TRANSACTION: 'TRANSACTION'
 }
 
 class PubSub {
-    constructor({blockchain}){
+    constructor({blockchain, transactionPool}){
 
         this.blockchain = blockchain;
+        this.transactionPool = transactionPool;
 
         this.publisher = redis.createClient();
         this.subscriber = redis.createClient();
@@ -24,8 +26,16 @@ class PubSub {
         console.log(`Message recieved. Channel: ${channel}. Message: ${message}.`)
 
         const parsedMessage = JSON.parse(message)
-        if(channel === CHANNELS.BLOCKCHAIN){
-            this.blockchain.replaceChain(parsedMessage);
+        
+        switch (channel){
+            case CHANNELS.BLOCKCHAIN:
+                this.blockchain.replaceChain(parsedMessage);
+                break;
+            case CHANNELS.TRANSACTION:
+                this.transactionPool.setTransaction(parsedMessage)
+                break;
+            default:
+                return;
         }
     }
 
@@ -48,6 +58,13 @@ class PubSub {
         this.publish({
             channel: CHANNELS.BLOCKCHAIN,
             message: JSON.stringify(this.blockchain.chain)
+        })
+    }
+
+    broadcastTransaction(transaction){
+        this.publish({
+            channel: CHANNELS.TRANSACTION,
+            message: JSON.stringify(transaction)
         })
     }
 
--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const blockchain = new Blockchain();
 const transactionPool = new TransactionPool();
 const wallet = new Wallet();
-const pubsub = new PubSub({blockchain});
+const pubsub = new PubSub({blockchain, transactionPool});
 
 const DEFAULT_PORT = 3000;
 const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`
@@ -49,17 +49,31 @@ app.post('/api/transact', (req, res) => {
     }
 
     transactionPool.setTransaction(transaction)
-    console.log('transactionPool', (transactionPool))
+    // console.log('transactionPool', (transactionPool))
 
-    res.json({type: "success", transaction})
+    pubsub.broadcastTransaction(transaction);
+    res.json({type: "success", transaction});
 })
 
-const syncChains = () => {
+app.get('/api/transaction-pool-map', (req, res) => {
+    res.json(transactionPool.transactionMap);
+})
+
+const syncWithRootState  = () => {
     request({url: `${ROOT_NODE_ADDRESS}/api/blocks`}, (error, response, body) => {
         if (!error && response.statusCode === 200){
             const rootChain = JSON.parse(body)
             console.log('replace chain on a sync with:', rootChain)
             blockchain.replaceChain(rootChain);
+        }
+    })
+
+    request({url: `${ROOT_NODE_ADDRESS}/api/transaction-pool-map`}, (error, response, body) => {
+        if (!error && response.statusCode === 200){
+            const rootTransactionPoolMap = JSON.parse(body);
+            console.log('replace transaction pool map on a sync with:', rootTransactionPoolMap);
+            transactionPool.setMap(rootTransactionPoolMap);
+
         }
     })
 }
@@ -75,6 +89,6 @@ const PORT = PEER_PORT || DEFAULT_PORT;
 app.listen(PORT, () => {
     console.log(`App listening at localhost:${PORT}`);
     if(PORT !== DEFAULT_PORT){
-        syncChains();
+        syncWithRootState();
     }
 })
