3eab5ae74bf10835cdd613b8c78442bc319b6a27
Daniel Atuma <atumadann@gmail.com>
2021-10-22 12:14:08 +0100

    Validated transaction data incoming into the blockchain.
    Validated incoming transaction input balances.

--- blockchain/index.js
+++ blockchain/index.js
@@ -1,5 +1,8 @@
 const Block = require('./block');
+const Transaction = require('../wallet/transaction')
+const Wallet = require('../wallet')
 const {cryptoHash} = require('../util');
+const { REWARD_INPUT, MINING_REWARD } = require('../config');
 
 class Blockchain{
     constructor(){
@@ -52,6 +55,45 @@ class Blockchain{
         
         console.log('replacing chain with:', chain)
         this.chain = chain;
+    }
+
+    validTransactionData({chain}){
+        for(let i = 1; i < chain.length; i++){
+            const block = chain[i];
+            let rewardTransactionCount = 0;
+
+            for (let transaction of block.data){
+                if(transaction.input.address === REWARD_INPUT.address){
+                    rewardTransactionCount += 1
+
+                    if(rewardTransactionCount > 1){
+                        console.error('Miner Rewards exceed input')
+                        return false;
+                    }
+
+                    if(Object.values(transaction.outputMap)[0] !== MINING_REWARD){
+                        console.error('Miner reward amount is invalid');
+                        return false;
+                    }
+                }else{
+                    if (!Transaction.validTransaction(transaction)){
+                        console.error('Invalid transaction')
+                        return false;
+                    }
+
+                    const trueBalance = Wallet.calculateBalance({
+                        chain: this.chain,
+                        address:    transaction.input.address
+                    });
+                    if(transaction.input.amount !== trueBalance){
+                        console.error('Invalid Input Amount');
+                        return false;
+                    }
+                }
+            }
+        }
+        
+        return true;
     }
 }
 
